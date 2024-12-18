2ef8603ddc325416479f749d923c0fd6cf4e08fa
Syceus1212 <notrandom20001@gmail.com>
2024-12-18 22:04:42 +0530

    Update hellofunc.c

--- hellofunc.c
+++ hellofunc.c
@@ -3,7 +3,7 @@
 
 void myPrintHelloMake(void) {
 
-  printf("Hello makefiles! This git repo is a fork for the ropper's computing fundamentals course by Syceus1212\n");
+  printf("Hello makefiles!This git repo is a fork for the ropper's computing fundamentals course by Syceus1212\n");
 
   return;
 }
